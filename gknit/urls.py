cce9580dcca9a9400dc78e2ffd59eb7813cda735
carlalennon lennon <carlalennon@gmail.com>
2024-01-14 20:00:09 +0000

    Make pattern_page.html

--- gknit/urls.py
+++ gknit/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from pattern.views import pattern_page
+from django.urls import path, include
 
 urlpatterns = [
-    path('pattern/', pattern_page, name='pattern_page'),
+    path('', include('pattern.urls'), name='pattern_page'),
     path('admin/', admin.site.urls),
 ]
