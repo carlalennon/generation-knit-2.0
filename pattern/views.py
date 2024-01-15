381a39fd18ece5d20116e92aa6ec16c33a4ce10d
carlalennon lennon <carlalennon@gmail.com>
2024-01-15 22:12:57 +0000

    Start on templates for pattern page

--- pattern/views.py
+++ pattern/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Pattern
+from django.http import HttpResponse
 
 # Create your views here.
 class PatternView(generic.ListView):
     queryset = Pattern.objects.all()
-    template_name = "pattern_list.html"
+    template_name = "pattern/index.html"
+    paginate_by = 3
+
+def patterns(request):
+    myPatterns = Pattern.objects.all()
+    template = loader.get_template('index.html')
+    context = {
+        'myPatterns' : myPatterns,
+    }
+    return HttpResponse(template.render(context, request))
