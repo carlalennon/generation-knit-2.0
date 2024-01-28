b3d844ed5a5a797da23d67cf94dc42b525323975
carlalennon lennon <carlalennon@gmail.com>
2024-01-28 00:21:31 +0000

    Users can upload an image to their profile

--- profiles/models.py
+++ profiles/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from sorl.thumbnail import ImageField
 
 
 
@@ -11,7 +12,7 @@ class Profile(models.Model):
         on_delete=models.CASCADE,
         related_name="profile"
     )
-
+    image = ImageField(upload_to="profiles")
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
