09f437e8a96930ac68399c8f99450166d8520157
carlalennon lennon <carlalennon@gmail.com>
2024-01-29 21:29:08 +0000

    FileField no longer mandatory, default image added to pattern

--- pattern/forms.py
+++ pattern/forms.py
@@ -5,6 +5,6 @@ from django_summernote.widgets import SummernoteWidget
 
 class PostPattern(forms.Form):
     title = forms.CharField(max_length=200)
-    image = forms.FileField()
+    image = forms.FileField(required=False)
     content = forms.CharField(widget=SummernoteWidget)
     #author = 
