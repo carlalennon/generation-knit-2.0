7f7c8aae3bdf823241ea05025e80329045254a0d
carlalennon lennon <carlalennon@gmail.com>
2024-01-30 21:59:24 +0000

    Start on user profile page

--- gknit/urls.py
+++ gknit/urls.py
@@ -23,12 +23,14 @@ from django.urls import path, include, re_path
 
 from feed import urls as feed_urls
 from pattern import urls as pattern_urls
+from profiles import urls as profile_urls
 
 urlpatterns = [
     path('summernote/', include('django_summernote.urls')),
     path('admin/', admin.site.urls),
     path('', include(feed_urls), name='feed'),
     path('', include(pattern_urls), name='patterns'),
+    path('profile/', include(profile_urls), name='profile'),
     re_path('', include("allauth.urls"))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
