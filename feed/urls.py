bbc9a8c6415f33883b133f4bba14ddad754486be
carlalennon lennon <carlalennon@gmail.com>
2024-02-01 21:30:16 +0000

    Get 500 server error from django, try amending to feed/ to solve

--- feed/urls.py
+++ feed/urls.py
@@ -5,6 +5,6 @@ from .views import HomePageView, PatternDetailView
 app_name = 'feed'
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='feed'),
+    path('feed/', HomePageView.as_view(), name='feed'),
     path('pattern/<int:pk>/', PatternDetailView.as_view(), name="detail"),
 ]
