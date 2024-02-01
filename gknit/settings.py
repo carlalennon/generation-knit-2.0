42572f3a43bb3059d54283d87a2db0159c919c81
carlalennon lennon <carlalennon@gmail.com>
2024-02-01 21:35:28 +0000

    Set debug to true for troubleshooting

--- gknit/settings.py
+++ gknit/settings.py
@@ -36,7 +36,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-carlalennon-generationk-qjyl64qx0g1.ws-eu107.gitpod.io','.herokuapp.com']
 
