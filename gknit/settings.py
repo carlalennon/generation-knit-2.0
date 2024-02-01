bb310adc9b099511ff92e49ad8cab9dd1d9b2065
carlalennon lennon <carlalennon@gmail.com>
2024-02-01 21:09:27 +0000

    Create procfile, add heroku to allowed hosts

--- gknit/settings.py
+++ gknit/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-carlalennon-generationk-qjyl64qx0g1.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['8000-carlalennon-generationk-qjyl64qx0g1.ws-eu107.gitpod.io','.herokuapp.com']
 
 
 # Application definition
