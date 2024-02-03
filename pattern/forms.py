e04c5fe315816999634a9b13eda6f17f857eb7a8
carlalennon lennon <carlalennon@gmail.com>
2024-02-03 18:12:15 +0000

    Change form to ModelForm

--- pattern/forms.py
+++ pattern/forms.py
@@ -3,20 +3,7 @@ from django_summernote.widgets import SummernoteWidget
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Pattern
 
-class PostPattern(forms.Form):
+class PostPattern(forms.ModelForm):
     title = forms.CharField(max_length=200)
     image = forms.FileField(required=False)
     content = forms.CharField(widget=SummernoteWidget)
-
-def edit_pattern(request, pattern_id):
-    pattern = get_object_or_404(Pattern, id=pattern_id)
-    if request.method == 'POST':
-        form = PostPattern(request.POST, request.FILES, instance=pattern)
-        if form.is_valid():
-            form.save()
-            return redirect('pattern_detail', pattern_id=pattern.id)
-    else:
-        form = PostPattern(instance=pattern)
-    return render(request, 'edit_pattern.html', {'form': form})
-
-
