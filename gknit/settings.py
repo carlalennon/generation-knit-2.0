b0bb3a5fd73b32aeb4a87a78db9af3dfb0e4f796
carlalennon lennon <carlalennon@gmail.com>
2024-02-03 15:05:05 +0000

    Remove allauth files from includes

--- gknit/settings.py
+++ gknit/settings.py
@@ -36,9 +36,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-carlalennon-generationk-qjyl64qx0g1.ws-eu107.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com']
 
 
 # Application definition
