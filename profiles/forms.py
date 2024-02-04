20255f128e5cb1b171fd08655aec5440affa9ac0
carlalennon lennon <carlalennon@gmail.com>
2024-02-04 21:57:32 +0000

    Fixed bug with allauth authetication emaail. Users can now sign up from the front page

---
+++ profiles/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django_summernote.widgets import SummernoteWidget
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Pattern
+
+class PostPattern(forms.ModelForm):
+    title = forms.CharField(max_length=200)
+    image = forms.FileField(required=False)
+    content = forms.CharField(widget=SummernoteWidget)
+
+    class Meta:
+        model = Pattern
+        fields = ['title', 'image', 'content']
