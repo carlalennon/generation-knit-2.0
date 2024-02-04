1900f58fcf32919ca6eebbf93e28a65ec67fa4eb
carlalennon lennon <carlalennon@gmail.com>
2024-02-04 16:09:44 +0000

    Create search view

--- search/views.py
+++ search/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from pattern.models import Pattern
 
 # Create your views here.
+class SearchVieww(ListView):
+    model = Pattern
+    template_name = 'search.html'
