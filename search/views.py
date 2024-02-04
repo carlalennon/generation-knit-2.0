b1fbda9b873155c6d84f5063b3ec4e47cb469570
carlalennon lennon <carlalennon@gmail.com>
2024-02-04 16:42:58 +0000

    Preliminary search build

--- search/views.py
+++ search/views.py
@@ -6,3 +6,11 @@ from django.views.generic import ListView
 class SearchView(ListView):
     model = Pattern
     template_name = 'search.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('p')
+        pattern_search = Pattern.objects.filter(
+            Q(name__icontains=query)
+        )
+        return pattern_search
+    
