a073af108bf03aca80568a20f26878cfaa334b85
carlalennon lennon <carlalennon@gmail.com>
2024-02-17 14:44:57 +0000

    Restore broken views function

--- feed/urls.py
+++ feed/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from views import HomePageView, PatternDetailView
+from .views import HomePageView, PatternDetailView
 
 app_name = 'feed'
 
 urlpatterns = [path('', HomePageView.as_view(), name='feed'),
-               path('pattern/<int:pk>/', PatternDetailView.as_view(),
-               name='detail')]
+               path('pattern/<int:pk>/', PatternDetailView.as_view(), name='detail')]
