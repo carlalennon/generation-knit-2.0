017f622096415c7de26767af890f114588ce566f
carlalennon lennon <carlalennon@gmail.com>
2024-04-30 23:06:34 +0000

    Reconfigured env.py

--- gknit/settings.py
+++ gknit/settings.py
@@ -32,9 +32,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
