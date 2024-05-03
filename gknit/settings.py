2c91f7f7feaa51e091402f14f0887bde158c7c87
carlalennon lennon <carlalennon@gmail.com>
2024-05-03 23:01:49 +0000

    Revert "Add new nav bar to large screens, remove user info from small screens"
    This reverts commit c2e397b83b20a39351b12b352635dbe854e30b37.

--- gknit/settings.py
+++ gknit/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
