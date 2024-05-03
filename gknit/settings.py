6ea6c7d8ef685bcbbed1ac1bf85c31493085f141
carlalennon lennon <carlalennon@gmail.com>
2024-05-03 23:02:31 +0000

    Revert "Reconfigured env.py"
    This reverts commit 017f622096415c7de26767af890f114588ce566f.

--- gknit/settings.py
+++ gknit/settings.py
@@ -32,9 +32,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.gitpod.io','.herokuapp.com']
 
 
 # Application definition
